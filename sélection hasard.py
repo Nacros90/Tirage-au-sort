7b6dbbda68995ad688dece807502b6e8cf90f215
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-06-03 16:55:42 +0200

    Update sélection hasard.py

--- sélection hasard.py
+++ sélection hasard.py
@@ -1,4 +1,7 @@
 import random
-eff=int(input("nombres de personnes : "))
-for x in range(0,eff):
-    nb=str(input("Nom de la personnes n°",x,": "))
+nb=int(input("nombres de personnes : "))
+for x in range(0,nb):
+    nom=str(input("Nom de la personnes n°",x,": "))
+for k in range(0,nb):
+    select=random.randint(1,nb)
+    
