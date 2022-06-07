3ff9539a3ac7f9a1b0fe827043975eb5ea22d369
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-06-07 23:18:45 +0200

    Injection des liste
    J'utilise les liste pour pouvoir lier des chiffres random à une string rentré manuellement, mais problème à la ligne 4 avec la boucle borné.

--- Tirage au sort.py
+++ Tirage au sort.py
@@ -1,6 +1,11 @@
 import random
+Tirage=[]
 nb=int(input("nombres de personnes : "))
 for x in range(0,nb):
     nom=str(input("Nom de la personnes n°",x,": "))
+    Tirage.append(nom)
 for k in range(0,nb):
     select=random.randint(1,nb)
+    Tirage[select]
+    del Tirage[select]
+    nb=nb-1
