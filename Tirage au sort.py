94172407d25c448353e926a72c873f40738f354b
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-06-15 13:50:52 +0200

    Réglage du problème à la ligne 5
    Il y a un nouveau problème à la ligne 9

--- Tirage au sort.py
+++ Tirage au sort.py
@@ -2,10 +2,10 @@ import random
 Tirage=[]
 nb=int(input("nombres de personnes : "))
 for x in range(0,nb):
-    nom=str(input("Nom de la personnes n°",x,": "))
+    nom=str(input("Nom de la personnes: "))
     Tirage.append(nom)
 for k in range(0,nb):
     select=random.randint(1,nb)
-    Tirage[select]
-    del Tirage[select]
-    nb=nb-1
+    Tirage[select]                                                  #problème à la ligne 9 je pense que c'est
+    nb=nb-1                                                         #l'assignation de la string dans la liste
+print("L'ordre des personnes est",Tirage,".")
