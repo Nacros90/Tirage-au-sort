4309ed30c08f5a3837a325a89db5ce1a758e6025
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-06-23 15:09:17 +0200

    Modif de l'ordre de certaines instruction

--- Tirage au sort.py
+++ Tirage au sort.py
@@ -5,8 +5,9 @@ for x in range(0,nb):
     nom=str(input("Nom de la personnes: "))
     Tirage.append(nom)
 print(Tirage)
+print("l'ordre des personnes est :")
 for k in range(0,nb):
     select=random.randint(1,nb)
     Tirage[select]                                                  #problème à la ligne 9 je pense que c'est
     nb=nb-1                                                         #l'assignation de la string dans la liste
-print("L'ordre des personnes est",Tirage,".")
+    print(Tirage[select])
