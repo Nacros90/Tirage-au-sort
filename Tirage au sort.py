647c6f09bb94c7090008347d88201f864bfff7f1
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-06-23 14:39:09 +0200

    Verif

--- Tirage au sort.py
+++ Tirage au sort.py
@@ -4,6 +4,7 @@ nb=int(input("nombres de personnes : "))
 for x in range(0,nb):
     nom=str(input("Nom de la personnes: "))
     Tirage.append(nom)
+print(Tirage)
 for k in range(0,nb):
     select=random.randint(1,nb)
     Tirage[select]                                                  #problème à la ligne 9 je pense que c'est
