e47a1c8ebe58baadf5a95cf494a7522094ddd945
Nacros90 <nael.crosnier@gmail.com>
2022-10-17 15:16:05 +0200

    modification des bornes de certaines boucles
    modif des bornes de certaines boucles
    
    il n'y à plus l'aire de problème pour la suppression des élément de la liste, mais plus pour les tirages avec un nombres inférieurs à cinq itérations

--- Tirage au sort.py
+++ Tirage au sort.py
@@ -4,10 +4,10 @@ nb=int(input("nombres de personnes : "))
 for x in range(0,nb):
     nom=str(input("Nom de la personnes: "))
     Tirage.append(nom)
-print(Tirage)
+"""print(Tirage)"""
 print("l'ordre des personnes est :")
-for k in range(0,nb):
+for k in range(1,nb):
     select=random.randint(1,nb)
     nb=nb-1
     print(Tirage[select])
-    del Tirage[select]                                              #Il y a un problème lors de la suppression d'un élement déjà tiré
+    del Tirage[select]                                              #Il y a un problème pour le nombre de tirage, une sortie de "range" est indiqué
