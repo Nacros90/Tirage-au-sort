2012f3a83f6edd415a1dac60d872a2fbe209fc15
Nacros90 <nael.crosnier@gmail.com>
2024-02-26 19:01:36 +0100

    Dernière version avant une nouvelle manière d'aborder les choses
    Ma nouvelle manière d'aborder le problème est d'utiliser 3 listes au lieu d'une seule. la première sera celle déjà existante mais on ne fera que la lire, la deuxième sera une liste dans laquelle ont y mettra tous les index déjà tiré afin de la les exclure pour les tirage suivants, la troisième liste sera le résultat final, ont mettra le nouvelle ordre de noms.

--- Tirage au sort.py
+++ Tirage au sort.py
@@ -2,13 +2,15 @@ import random
 Tirage=[]
 nb=int(input("Nombres de personnes : "))
 for x in range(0,nb):
-    print("Personne",x,":")
+    print("Personne",x+1,":")
     nom=str(input("Nom de la personnes : "))
     Tirage.append(nom)
 print(Tirage)
 print("L'ordre des personnes est :")
-for k in range(1,nb):
+for k in range(1,nb+1):
+    print(k)
     select=random.randint(1,nb)
+    print(select)
     nb=nb-1
-    print(Tirage[select])
+    print(Tirage[select+1])
     del Tirage[select]                                              #Il y a un problème pour le nombre de tirage, une sortie de "range" est indiqué
