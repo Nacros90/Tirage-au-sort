45be1fe15889600abc25bb75ce263dc12cd4a4de
Nacros90 <nael.crosnier@gmail.com>
2024-02-26 18:07:08 +0100

    Update Tirage au sort.py

--- Tirage au sort.py
+++ Tirage au sort.py
@@ -1,11 +1,12 @@
 import random
 Tirage=[]
-nb=int(input("nombres de personnes : "))
+nb=int(input("Nombres de personnes : "))
 for x in range(0,nb):
-    nom=str(input("Nom de la personnes: "))
+    print("Personne",x,":")
+    nom=str(input("Nom de la personnes : "))
     Tirage.append(nom)
-"""print(Tirage)"""
-print("l'ordre des personnes est :")
+print(Tirage)
+print("L'ordre des personnes est :")
 for k in range(1,nb):
     select=random.randint(1,nb)
     nb=nb-1
