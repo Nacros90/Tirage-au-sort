9c61debfe5ec08c5bea3036d59abf6369b5210ec
Nacros90 <nael.crosnier@gmail.com>
2024-02-26 20:54:22 +0100

    Essaie d'une nouvelle et découverte d'une nouvelle fonction
    Début de l'essaie de la nouvelle méthode. J'ai d'ailleurs découvert une nouvelle fonction qui permet de réorganiser une liste de manière aléatoire c-à-d le cœur même de mon programme et de tous les problème rencontrés.
    Cependant, pour des raisons pédagogique ainsi que de "défi" je vais tenter de coder le programme sans cette fonction.

--- Tirage au sort.py
+++ Tirage au sort.py
@@ -1,16 +1,43 @@
 import random
-Tirage=[]
+Noms=[]
+#Première partie du programme, l'entré des noms
 nb=int(input("Nombres de personnes : "))
 for x in range(0,nb):
     print("Personne",x+1,":")
     nom=str(input("Nom de la personnes : "))
-    Tirage.append(nom)
-print(Tirage)
+    Noms.append(nom)
+print(Noms)
+
+
+
+random.shuffle(Noms)                #La fonction random.shuffle(x) permet de mélanger de manière aléatoire une liste
+print(Noms)
+
+
+
+"""
+#Deuxième partie du programme, classement aléatoire des noms
+index=[]
+ordrefin=[]
+for i in range(1,nb+1):
+    print(i,"vérif 1")
+    sel=random.randint(1,nb)
+    if sel in index:
+        sel=random.randint
+    index.append(sel)
+    print(sel,"vérif 2")
+    nb=nb-1
+"""
+
+
+"""
+                #Ancienne version qui présente trop de pb
 print("L'ordre des personnes est :")
 for k in range(1,nb+1):
     print(k)
     select=random.randint(1,nb)
     print(select)
     nb=nb-1
-    print(Tirage[select+1])
-    del Tirage[select]                                              #Il y a un problème pour le nombre de tirage, une sortie de "range" est indiqué
+    print(Noms[select+1])
+    del Noms[select]                          #Il y a un problème pour le nombre de tirage, une sortie de "range" est indiqué
+"""
